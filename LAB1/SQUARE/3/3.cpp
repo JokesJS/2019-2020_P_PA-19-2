dfd071098a0972f1235b4281c5d6015853250d17
JokesJS <54980798+JokesJS@users.noreply.github.com>
2019-10-07 20:30:51 +0300

    Add files via upload

---
+++ LAB1/SQUARE/3/3.cpp
@@ -0,0 +1,17 @@
+﻿#include "stdafx.h"
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[]);
+int main ()
+{
+	cout <<"-----------"<<endl;
+	cout <<"|         |"<<endl;
+	cout <<"|         |"<<endl;
+	cout <<"|         |"<<endl;
+	cout <<"|         |"<<endl;
+	cout <<"-----------"<<endl;
+	system("pause");
+	return 0;
+}
+
